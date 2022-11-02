8c495dac9505954b8e01712124ce930c68b3d7b5
thatlittlemoonfromstarwars <andor.siegers@gmail.com>
2022-11-02 15:38:03 -0400

    Update Main.c

--- Main.c
+++ Main.c
@@ -12,17 +12,39 @@ D - right drive wheel
 Sensor Ports:
 1 - touch
 2 - gyro
-3 - colour
+3 - color
 4 - ultrasonic
 */
 void configureAllSensors();
 void dispenseDomino();
-void followLine();
-void navFromCoords();
-void calcPath();
+void followLine(); // Sean
+void followPathFromFile(); // Andor
+void driveToStartLocation(); // Andor
+void calcPath(); // Andor
 
 task main()
 {
-	motor[motorA] = motor[motorD] = 50;
-	wait1Msec(5000000000);
+	const int MAX_DOMINO_LOAD = 60;
+
+	configureAllSensors();
+	int dominoCount = MAX_DOMINO_LOAD;
 }
+
+void configureAllSensors()
+{
+	SensorType[S1] = sensorEV3_Touch;
+	SensorType[S2] = sensorEV3_Gyro;
+	wait1Msec(50);
+	SensorType[S3] = sensorEV3_Color;
+	wait1Msec(50);
+	SensorType[S4] = sensorEV3_Ultrasonic;
+	wait1Msec(50);
+	SensorMode[S2] = modeEV3Gyro_Calibration;
+	wait1Msec(50);
+	SensorMode[S3] = modeEV3Color_Color;
+	wait1Msec(100);
+	SensorMode[S2] = modeEV3Gyro_RateAndAngle;
+	wait1Msec(50);
+
+}
+
