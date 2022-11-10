a448d0bd6f6ad2031b1936caea894f65f9c2b579
thatlittlemoonfromstarwars <andor.siegers@gmail.com>
2022-11-10 15:51:47 -0500

    Update TestProgram.c

--- TestProgram.c
+++ TestProgram.c
@@ -1,143 +1,145 @@
-void configureSensors();
+void configureAllSensors();
 
-
-task main()
-{
-bool piston_arm = false;
-bool door = false;
-bool drive = false;
 const int PISTON_CODE_POWER = 75;
 const int DOOR_POWER = 10;
 const int DOOR_UP_LIMIT = 720;
 const int DOOR_DOWN_LIMIT = 0;
 const int DRIVE_CODE_POWER = 25;
 
-displayString(5, "Press enter for piston arm");
-displayString(7, "Press left for door");
-displayString(9, "Press Right for drive forward");
-
-
-while(!getButtonPress(buttonAny))
-{}
-
-if(getButtonPress(buttonEnter))
+task main()
 {
-	piston_arm = true;
-}
-if(getButtonPress(buttonLeft))
-{
-	door = true;
-}
-if(getButtonPress(buttonRight))
-{
-	drive = true;
-}
-while(getButtonPress(buttonAny))
-{}
+	bool piston_arm = false;
+	bool door = false;
+	bool drive = false;
 
-displayString.clear();
+	displayString(5, "Press enter for piston arm");
+	displayString(7, "Press left for door");
+	displayString(9, "Press Right for drive forward");
 
-if(piston_arm)
-{
-	displayString(5, "Press down for 5% power");
-	displayString(7, "Press left for 25% power");
-	displayString(9, "press enter for code power");
-	displayString(11, "Press right for 50% power");
-	displayString(13, "Press up for 100% power");
-
-	while(!getButtonPress(buttonAny))
-	{}
-
-	if(getButtonPress(buttonDown))
+	configureAllSensors();
+	while(true)
 	{
-		motor[motorB] = 5;
-	}
-	if(getButtonPress(buttonLeft))
-	{
-		motor[motorB] = 25;
-	}
-	if(getButtonPress(buttonEnter))
-	{
-		motor[motorB] = PISTON_CODE_POWER;
-	}
-	if(getButtonPress(buttonRight))
-	{
-		motor[motorB] = 50;
-	}
-	if(getButtonPress(buttonUp))
-	{
-		motor[motorB] = 100;
-	}
-
-	wait1Msec(1000)
-	displayString.clear();
-	displayString(7, "Press enter to stop");
-	while(!getButtonPress(buttonEnter))
-	{}
-	motor[motorB] = 0;
-}
-
-
-if(door)
-{
-	displayString(7, "Press enter to stop");
-
-	while(!getButtonPress(buttonEnter))
-	{
-		motor[motorC] = DOOR_POWER;
-		while(nMotorEncoder[motorC] < DOOR_UP_LIMIT)
+		while(true)
+		{
+			if(getButtonPress(buttonEnter))
+			{
+				piston_arm = true;
+				break;
+			}
+			else if(getButtonPress(buttonLeft))
+			{
+				door = true;
+				break;
+			}
+			else if(getButtonPress(buttonRight))
+			{
+				drive = true;
+				break;
+			}
+		}
+		while(getButtonPress(buttonAny))
 		{}
 
-		motor[motorC] = -DOOR_POWER;
-		while(nMotorEncoder[motorC] > DOOR_DOWN_LIMIT)
-		{}
+		eraseDisplay();
+
+		if(piston_arm)
+		{
+			displayString(5, "Press down for 5% power");
+			displayString(7, "Press left for 25% power");
+			displayString(9, "press enter for code power");
+			displayString(11, "Press right for 50% power");
+			displayString(13, "Press up for 100% power");
+
+			while(true)
+			{
+				if(getButtonPress(buttonDown))
+				{
+					motor[motorB] = 5;
+				}
+				else if(getButtonPress(buttonLeft))
+				{
+					motor[motorB] = 25;
+				}
+				else if(getButtonPress(buttonEnter))
+				{
+					motor[motorB] = PISTON_CODE_POWER;
+				}
+				else if(getButtonPress(buttonRight))
+				{
+					motor[motorB] = 50;
+				}
+				else if(getButtonPress(buttonUp))
+				{
+					motor[motorB] = 100;
+				}
+			}
+
+			wait1Msec(1000);
+			eraseDisplay();
+			displayString(7, "Press enter to stop");
+			while(!getButtonPress(buttonEnter))
+			{}
+			motor[motorB] = 0;
+
+		}
+		else if(door)
+		{
+			displayString(7, "Press enter to stop");
+
+			while(!getButtonPress(buttonEnter))
+			{
+				motor[motorC] = DOOR_POWER;
+				while(nMotorEncoder[motorC] < DOOR_UP_LIMIT)
+				{}
+
+				motor[motorC] = -DOOR_POWER;
+				while(nMotorEncoder[motorC] > DOOR_DOWN_LIMIT)
+				{}
+			}
+			motor[motorC] = 0;
+			eraseDisplay();
+
+		}
+		else if(drive)
+		{
+			displayString(5, "Press down for 5% power");
+			displayString(7, "Press left for 25% power");
+			displayString(9, "press enter for code power");
+			displayString(11, "Press right for 50% power");
+			displayString(13, "Press up for 100% power");
+
+			while(!getButtonPress(buttonAny))
+			{}
+
+			if(getButtonPress(buttonDown))
+			{
+				motor[motorA] = motor[motorD] = 5;
+			}
+			if(getButtonPress(buttonLeft))
+			{
+				motor[motorA] = motor[motorD] = 25;
+			}
+			if(getButtonPress(buttonEnter))
+			{
+				motor[motorA] = motor[motorD] = DRIVE_CODE_POWER;
+			}
+			if(getButtonPress(buttonRight))
+			{
+				motor[motorA] = motor[motorD] = 50;
+			}
+			if(getButtonPress(buttonUp))
+			{
+				motor[motorA] = motor[motorD] = 100;
+			}
+
+			wait1Msec(1000);
+			eraseDisplay();
+			displayString(7, "Press enter to stop");
+			while(!getButtonPress(buttonEnter))
+			{}
+			motor[motorA] = motor[motorB] = 0;
+		}
 	}
-	motor[motorC] = 0;
-	displayString.clear();
-}
-
-
-if(drive)
-{
-	displayString(5, "Press down for 5% power");
-	displayString(7, "Press left for 25% power");
-	displayString(9, "press enter for code power");
-	displayString(11, "Press right for 50% power");
-	displayString(13, "Press up for 100% power");
-
-	while(!getButtonPress(buttonAny))
-	{}
-
-	if(getButtonPress(buttonDown))
-	{
-		motor[motorA] = motor[motorD] = 5;
-	}
-	if(getButtonPress(buttonLeft))
-	{
-		motor[motorA] = motor[motorD] = 25;
-	}
-	if(getButtonPress(buttonEnter))
-	{
-		motor[motorA] = motor[motorD] = DRIVE_CODE_POWER;
-	}
-	if(getButtonPress(buttonRight))
-	{
-		motor[motorA] = motor[motorD] = 50;
-	}
-	if(getButtonPress(buttonUp))
-	{
-		motor[motorA] = motor[motorD] = 100;
-	}
-
-	wait1Msec(1000);
-	displayString.clear();
-	displayString(7, "Press enter to stop");
-	while(!getButtonPress(buttonEnter))
-	{}
-	motor[motorA] = motor[motorB] = 0;
-}
-
-
 }
 
 void configureAllSensors()
